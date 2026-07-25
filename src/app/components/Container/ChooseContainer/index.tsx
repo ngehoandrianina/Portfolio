3661135ed5b46a294c9fb7715525169eecb43922
Antonio Claret <antonioclaret2004@gmail.com>
2026-07-25 20:59:20 +0300

    style: update ChooseContainer component for consistent text styling

--- src/app/components/Container/ChooseContainer/index.tsx
+++ src/app/components/Container/ChooseContainer/index.tsx
@@ -5,10 +5,10 @@ const ChooseContainer = ({icon,text,number}) => {
   return (
     <div className=' w-96 p-4'>
         <div className='flex justify-between items-center'>
-            <div className=''>{icon}</div>
-            <h1>{number}</h1>
+            <div className='text-white'>{icon}</div>
+            <h1 className='text-2xl text-white'>{number}</h1>
         </div>
-        <p className='mt-8'>{text}</p>
+        <p className='mt-8 text-white'>{text}</p>
     </div>
   )
 }
